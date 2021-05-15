bce0ee1f13c1fa2302e73f8acb22c04be4aa47a3
SanceiLaks <39582793+Sanceilaks@users.noreply.github.com>
2021-05-15 23:04:02 +0300

    site impoves

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,34 +1,86 @@
-var rev = "fwd";
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-function titlebar(val) {
-    var msg = "LemiProject";
-    var res = " ";
-    var speed = 1000;
-    var pos = val;
+function initializeBackground() {
+    VANTA.TOPOLOGY({
+    el: "#particles-js",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x009966
+    })
+}
 
-    var le = msg.length;
-    if (rev == "fwd") {
-        if (pos < le) {
-            pos = pos + 1;
-            scroll = msg.substr(0, pos);
-            document.title = scroll;
-            timer = window.setTimeout("titlebar(" + pos + ")", speed);
+function initializeTitle() {
+    var rev = "fwd";
+
+    function titlebar(val) {
+        var msg = "LemiProject";
+        var res = " ";
+        var speed = 1000;
+        var pos = val;
+
+        var le = msg.length;
+        if (rev == "fwd") {
+            if (pos < le) {
+                pos = pos + 1;
+                scroll = msg.substr(0, pos);
+                document.title = scroll;
+                timer = window.setTimeout("titlebar(" + pos + ")", speed);
+            } else {
+                rev = "bwd";
+                timer = window.setTimeout("titlebar(" + pos + ")", speed);
+            }
         } else {
-            rev = "bwd";
-            timer = window.setTimeout("titlebar(" + pos + ")", speed);
-        }
-    } else {
-        if (pos > 0) {
-            pos = pos - 1;
-            var ale = le - pos;
-            scrol = msg.substr(ale, le);
-            document.title = scrol;
-            timer = window.setTimeout("titlebar(" + pos + ")", speed);
-        } else {
-            rev = "fwd";
-            document.title = " ";
-            timer = window.setTimeout("titlebar(" + pos + ")", speed);
+            if (pos > 0) {
+                pos = pos - 1;
+                var ale = le - pos;
+                scrol = msg.substr(ale, le);
+                document.title = scrol;
+                timer = window.setTimeout("titlebar(" + pos + ")", speed);
+            } else {
+                rev = "fwd";
+                document.title = " ";
+                timer = window.setTimeout("titlebar(" + pos + ")", speed);
+            }
         }
     }
+    titlebar(0);
 }
-titlebar(0);
+
+function initializeAudio() {
+    document.getElementById("bgmusic").volume = 0.05;
+}
+
+function infinityTyper() {
+    let typingContent = [
+    "By people for people",
+    "Only free (joke)",
+    "Donate pls :/",
+    "Our users is best",
+    "Ok dude",
+    "If you see this - be happy",
+    "I love you ;)"
+    ];
+    var rand = getRandomInt(typingContent.length);
+    var element = getElementById("typingElement");
+    var typeWriter = new Typewriter(element, {loop: true});
+    typeWriter.typeString(typingContent[rand]).pauseFor(6000).deleteAll().pauseFor(500).callFunction(() => {
+        typeWriter.stop();
+        infinityTyper();
+    }).start();
+}
+
+function initialize(){
+    initializeAudio();
+    initializeTitle();
+    initializeBackground();
+    infinityTyper();
+}
+
+initialize();
